4ac74ae6a85b71136b076a391ac28898f8b107ba
VICTOR RUIZ <veruiz@uc.cl>
2025-04-29 18:32:07 -0400

    se agrega netlify al proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: netlify()
+});
